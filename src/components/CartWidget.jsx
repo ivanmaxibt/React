644e235df7708eaee3d970c167b4a4de1cdeb1a7
ivanmaxibt <107760930+ivanmaxibt@users.noreply.github.com>
2024-08-08 22:04:20 -0300

    Delete src directory

--- src/components/CartWidget.jsx
+++
@@ -1,7 +0,0 @@
-import changuito from "../assets/cart.png";
-
-export const NavBar = () =>(
-    <>
-     <img src="changuito"/>5
-    </>
-);
--- src/components/ItemListContainer.jsx
+++
@@ -1,1 +0,0 @@
-export const ItemListContainer = () => 1;
